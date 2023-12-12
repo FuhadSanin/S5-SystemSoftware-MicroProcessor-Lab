443e7a5ecff4735525f5444dd42a224618712101
FuhadSanin <fuhadsanin500@gmail.com>
2023-12-12 21:55:54 +0530

    Pass-2

--- System-Software/2-Pass-Assembler/PASS-2.c
+++ System-Software/2-Pass-Assembler/PASS-2.c
@@ -2,81 +2,79 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(){
-    FILE *fint,*fop,*fsym,*flen;
-    int i,len,start;
-    char label[20],opcode[20],operand[20],adr[20];
-    char code[20],mnemonic[20];
-    char symbol[20],locctr[20];
+int main()
+{
+    FILE *fint, *fop, *fsym, *flen;
+    int i, len, start;
+    char label[20], opcode[20], operand[20], adr[20];
+    char code[20], mnemonic[20];
+    char symbol[20], locctr[20];
 
     fint = fopen("intermediate.txt", "r");
     fop = fopen("optab.txt", "r");
     fsym = fopen("symtab.txt", "r");
     flen = fopen("length.txt", "r");
 
-    fscanf(fint,"\t%s\t%s\t%s",label,opcode,operand);
-    if(strcmp("START",opcode) == 0)
+    fscanf(fint, "\t%s\t%s\t%s", label, opcode, operand);
+    if (strcmp("START", opcode) == 0)
     {
         start = atoi(operand);
-        fscanf(flen,"%d",&len);
+        fscanf(flen, "%d", &len);
     }
-    printf("H^%s^00%s^%d",label,operand,start + len);
-    printf("T^00%s",operand);
+    printf("H^%s^00%s^00%d\n", label, operand, start + len);
+    printf("T^00%s^", operand);
 
-    fscanf(fint,"%s\t%s\t%s\t%d",adr,label,opcode,operand);
-    while (strcmp("END",operand)!=0)
+    fscanf(fint, "%s\t%s\t%s\t%s", adr, label, opcode, operand);
+    while (strcmp("END", opcode) != 0)
     {
-        fscanf(fop,"%s\t%s",code,mnemonic);
+        fscanf(fop, "%s\t%s", code, mnemonic);
         while (!feof(fop))
         {
-            if(strcmp(opcode,code) == 0){
+            if (strcmp(opcode, code) == 0)
+            {
                 fclose(fop);
-                fscanf(fsym,"%s\t%s",symbol,locctr);
+                fscanf(fsym, "%s\t%s", symbol, locctr);
                 while (!feof(fsym))
                 {
-                    if(strcmp(operand,symbol) == 0)
+                    if (strcmp(operand, symbol) == 0)
                     {
-                        printf("%s%s^",locctr,mnemonic);
+                        printf("%s%s^", locctr, mnemonic);
                         break;
-                    }else{
-                fscanf(fsym,"%s\t%s",symbol,locctr);
-
                     }
-
+                    else
+                    {
+                        fscanf(fsym, "%s\t%s", symbol, locctr);
+                    }
                 }
-            break;
-            }else{
-        fscanf(fop,"%s\t%s",code,mnemonic);
-
+                break;
             }
-            
+            else
+            {
+                fscanf(fop, "%s\t%s", code, mnemonic);
+            }
         }
-        if(strcmp("WORD",opcode) == 0){
-            printf("0000%s^",operand);
-
-        }else if(strcmp("BYTE",opcode) == 0){
+        if (strcmp("WORD", opcode) == 0)
+        {
+            printf("0000%s^", operand);
+        }
+        else if (strcmp("BYTE", opcode) == 0)
+        {
             len = strlen(operand);
-            for(i=2;i<len;i++){
-                printf("%d",operand[i]);
+            for (i = 2; i < len; i++)
+            {
+                printf("%d", operand[i]);
             }
             printf("^");
-
         }
-    fop = fopen("optab.txt", "r");
-    fscanf(fint,"%s\t%s\t%s\t%d",adr,label,opcode,&operand);
-    fseek(fop,SEEK_SET,0);
-
+        fop = fopen("optab.txt", "r");
+        fscanf(fint, "%s\t%s\t%s\t%s", adr, label, opcode, operand);
+        fseek(fop, SEEK_SET, 0);
     }
-    printf("E^00%s",operand);
-      fclose(fint);
+    printf("\nE^00%d\n", start);
+    fclose(fint);
     fclose(fop);
     fclose(fsym);
     fclose(flen);
-
-    
-
-    
-
 
     return 0;
 }
